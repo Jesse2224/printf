2de9e35e6f805fc991eaa7014204943ce8d03f5f
Jesse2224 <111058725+Jesse2224@users.noreply.github.com>
2022-10-18 22:04:25 +0100

    Update print_b.c

--- print_b.c
+++ print_b.c
@@ -5,15 +5,16 @@
 * @b: unsigned int i binary
 * Return: number of digit printed
 */
+
 int print_b(va_list b)
 {
 	unsigned int m, e = 2147483648, s, sum = 0;
 	unsigned int a[32];
-
 	int count;
 
 	m = va_arg(b, unsigned int);
 	a[0] = m / e;
+	
 	for (s = 1; s < 32; s++)
 	{
 		e /= 2;
